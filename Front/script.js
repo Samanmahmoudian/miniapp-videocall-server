7d1f6d2df37d4147eb71521a8fed463a03aa2743
Saman <samanmahmoudian.sm@gmail.com>
2025-02-19 08:38:58 +0330

    .

--- Front/script.js
+++ Front/script.js
@@ -23,36 +23,32 @@ const socket = io('http://localhost:3000');
 
 const peerConnectionConfig ={
     iceServers: [
-        // {
-        //     urls: "stun:stun.relay.metered.ca:80",
-        //   },
-        //   {
-        //     urls: "turn:global.relay.metered.ca:80",
-        //     username: "a4f5d501c33dfea6e2836653",
-        //     credential: "sxmhLRRVlHNc7aUL",
-        //   },
-        //   {
-        //     urls: "turn:global.relay.metered.ca:80?transport=tcp",
-        //     username: "a4f5d501c33dfea6e2836653",
-        //     credential: "sxmhLRRVlHNc7aUL",
-        //   },
-        //   {
-        //     urls: "turn:global.relay.metered.ca:443",
-        //     username: "a4f5d501c33dfea6e2836653",
-        //     credential: "sxmhLRRVlHNc7aUL",
-        //   },
-        //   {
-        //     urls: "turns:global.relay.metered.ca:443?transport=tcp",
-        //     username: "a4f5d501c33dfea6e2836653",
-        //     credential: "sxmhLRRVlHNc7aUL",
-        //   },
+        {
+            urls: "stun:stun.relay.metered.ca:80",
+          },
+          {
+            urls: "turn:global.relay.metered.ca:80",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+          },
+          {
+            urls: "turn:global.relay.metered.ca:80?transport=tcp",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+          },
+          {
+            urls: "turn:global.relay.metered.ca:443",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+          },
+          {
+            urls: "turns:global.relay.metered.ca:443?transport=tcp",
+            username: "a4f5d501c33dfea6e2836653",
+            credential: "sxmhLRRVlHNc7aUL",
+          },
         
-        // { urls: "stun:stun.l.google.com:19302" }
-    {
-                urls: "turn:2.176.229.75:3478",
-                username: "samanmahmoudian",
-                credential: "Saman1384",
-              },
+        { urls: "stun:stun.l.google.com:19302" }
+
     ],
   }
 

3bd0bb8184a5333a1a0994657af03ff89c74908c
Saman <samanmahmoudian.sm@gmail.com>
2025-03-02 16:04:23 +0330

    .

--- src/signaling/signaling.gateway.ts
+++ src/signaling/signaling.gateway.ts
@@ -4,7 +4,7 @@ import { query } from 'express';
 import { Server, Socket } from 'socket.io';
 let queue:any[] = []
 
-@WebSocketGateway()
+@WebSocketGateway({cors:true})
 export class SignalingGateway implements OnGatewayConnection , OnGatewayDisconnect{
   @WebSocketServer()
   server:Server

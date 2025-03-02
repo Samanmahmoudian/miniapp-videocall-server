cd484e350584732b8753e5c71370a9c6f33db3b3
Saman <samanmahmoudian.sm@gmail.com>
2025-03-02 15:09:15 +0330

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SignalingGateway } from './signaling/signaling.gateway';
-import { SignalingService } from './signaling/signaling.service';
+
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, SignalingGateway, SignalingService],
+  providers: [AppService, SignalingGateway],
 })
 export class AppModule {}
--- src/signaling/signaling.gateway.ts
+++ src/signaling/signaling.gateway.ts
@@ -1,112 +1,71 @@
 import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server , Socket} from 'socket.io';
-import { SignalingService } from './signaling.service';
-import { measureMemory } from 'vm';
-let first_state = ''
-let second_state = ''
+import { subscribe } from 'diagnostics_channel';
+import { query } from 'express';
+import { Server, Socket } from 'socket.io';
+let queue:any[] = []
 
-@WebSocketGateway({cors:{origin:'*'}})
-export class SignalingGateway implements OnGatewayDisconnect {
-constructor( private signalingService:SignalingService){}
-@WebSocketServer()
-server:Server
+@WebSocketGateway()
+export class SignalingGateway implements OnGatewayConnection , OnGatewayDisconnect{
+  @WebSocketServer()
+  server:Server
+  private clients = new Map()
 
-private clients = new Map()
-
-
-async newCall(client:Socket){
-  const checkState = await this.signalingService.check_state(client.id)
-  if(checkState == 'required'){
-    first_state =  client.id
-  }else if(checkState == 'connected'){
-    second_state =  client.id
-    if (first_state !== '') {
-      const target = await this.clients.get(first_state);
-      if (target) {
-          target.emit('offer_state', { state: 'ready', partnerId:  second_state });
-          client.emit('offer_state', { state: 'connected', partnerId:  first_state });
+    async handleConnection(client:Socket) {
+        const userTelegramId = await client.handshake.query.userTelegramId
+        if(this.clients.has(userTelegramId)){
+          await this.clients.delete(userTelegramId)
+        }
+        await this.clients.set(userTelegramId , client)
+        await this.startNewCall(userTelegramId)
       }
-  }else{
-    this.newCall(client)
-  }
-  }else{
-    this.newCall(client)
-  }
-}
+    async handleDisconnect(client:Socket) {
 
-
-
-@SubscribeMessage('readytostart')
-async handleStart(@ConnectedSocket() client:Socket){
-   this.clients.set(client.id , client)
-   client.emit('my_id' , client.id)
-   this.newCall(client)
-}
-
-async handleDisconnect(client: Socket) {
-  if(first_state == client.id){
-    first_state = ''
-  }else if(second_state == client.id){
-    second_state = ''
-  }
-  this.handleDisconnected(client.id , client)
-   this.clients.delete(client.id)
-
-}
-@SubscribeMessage('offer')
-async handleOffer(@MessageBody() message , @ConnectedSocket() client:Socket){
-  const target =  await this.clients.get(message.to)
-  if(target){
-    await target.emit('offer' , message.offer)
-  }
-
-}
-
-@SubscribeMessage('answer')
-async handleAnswer(@MessageBody() message , @ConnectedSocket() client:Socket){
-    const target = await  this.clients.get(message.to)
-    if(target){
-       target.emit('answer' , message.answer)
-    }else{
-      console.log('answer is undefined')
+        for (let [key , value] of this.clients.entries()){
+          if(value == client){
+            this.clients.delete(key)
+          }
+        }
     }
 
-}
+    async startNewCall(TelegramId){
+      if(queue.length == 0){
+        queue.push(TelegramId)
+      }else if(queue.length == 1){
+          const caller = await this.clients.get(TelegramId)
+          const callee = await this.clients.get(queue[0])
+          if(callee){
+            caller.emit('message' , {type: 'caller' , data: {partnerId: queue[0]}})
+            callee.emit('message' , {type: 'callee' , data: {partnerId: TelegramId}})
+            queue = []
+          }else{
+            this.startNewCall(TelegramId)
+          }
+      
+      }else if(queue.length > 1){
+        const caller = await this.clients.get(queue[0])
+        const callee = await this.clients.get(queue[1])
+        if(caller && callee){
+          await caller.emit('startNewCall' , {partnerId: queue[1]})
+          queue = []
+        }else{
+          this.startNewCall(TelegramId) 
+        }
 
-@SubscribeMessage('ice')
-async handleIce(@MessageBody() message , @ConnectedSocket() client:Socket){
-  const target =  await this.clients.get(message.to)
-  if(target){
-     target.emit('ice' , message.ice)
+    }
   }
-}
 
-@SubscribeMessage('nextcall')
-async handleNextcall(@MessageBody() message , @ConnectedSocket() client:Socket){
-const target =  await this.clients.get(message)
-if(target){
-   await target.emit('nextcall' , message)
-}
-  if(first_state == client.id){
-    first_state = ''
-  }else if(second_state == client.id){
-    second_state = ''
+  @SubscribeMessage('startNewCall')
+  async handleStartNewCall(@MessageBody() telegramId , @ConnectedSocket() client:Socket){
+    await this.startNewCall(telegramId)
   }
-}
-
-@SubscribeMessage('startnewcall')
-async handleStartNewCall( @ConnectedSocket() client:Socket){
-  await this.newCall(client)
 
 
-}
-
-
-@SubscribeMessage('disconnected')
-async handleDisconnected(@MessageBody() message , @ConnectedSocket() client:Socket){
-  client.broadcast.emit('disconnected' , message)
-
-
-}
+  @SubscribeMessage('message')
+  async handleOffer(@MessageBody() message , @ConnectedSocket() client:Socket){
+    const target =  await this.clients.get(message.to)
+    if(target){
+      await target.emit('message' , {type: message.type , data: message.data})
+    }
+  }
 
 }
